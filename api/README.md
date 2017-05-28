2e7c21953eea0e0ea8630436727dd68469bae866
Makoto Chiba <hypermkt@gmail.com>
2017-05-28 12:47:37 +0900

    Add get template list entrypoint with mock data.

---
+++ api/README.md
@@ -0,0 +1,5 @@
+
+### curl
+```sh
+$ curl -v http://localhost:1323/v1/website_templates | jq .
+```
---
+++ api/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+type WebsiteTemplate struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	e := echo.New()
+
+	e.GET("/v1/website_templates", func(c echo.Context) error {
+		templates := []WebsiteTemplate{
+			{Id: 1, Name: "Cool Template"},
+			{Id: 2, Name: "Cute Template"},
+		}
+		return c.JSON(http.StatusOK, templates)
+		//return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
